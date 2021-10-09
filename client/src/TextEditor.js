1b612d4f357a0c1f047f4618a8bef0d3f50171c5
kbarr <kbarretoranados@gmail.com>
2021-10-09 01:59:24 -0500

    CSS Styles

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -2,13 +2,30 @@ import "quill/dist/quill.snow.css"
 import { useCallback } from 'react'
 import Quill from 'quill'
 
+// Toolbar Options 
+const toolbarOptions = [
+    ['bold', 'italic', 'underline'],                  // toggled buttons
+    ['link','image', 'blockquote', 'code-block'],
+
+    [{ 'list': 'ordered'}, { 'list': 'bullet' }],     // ustom button values
+  
+    [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+  
+    [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+    [{ 'font': [] }],
+    [{ 'align': [] }],
+  
+    ['clean']                                         // remove formatting button
+  ];
+
 export default function TextEditor() {
     const wrapperRef = useCallback(wrapper => {
         if (wrapper == null) return
         wrapper.innerHTML = ""
         const editor = document.createElement("div")
         wrapper.append(editor)
-        new Quill(editor, { theme: "snow" })
+        new Quill(editor, { theme: "snow", modules: {toolbar: toolbarOptions} })
     }, [])
-    return <div id="container" ref={wrapperRef}></div>
+    return <div className="container" ref={wrapperRef}></div>
 }
